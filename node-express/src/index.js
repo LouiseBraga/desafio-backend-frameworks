d8cbdb96efa24c34192b0f5ee810f798d64931ab
Louise <bragamlouise@gmail.com>
2026-04-24 10:45:36 -0300

    Ambiente validado: Node e Java rodando com sucesso

--- node-express/src/index.js
+++ node-express/src/index.js
@@ -5,4 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(userRoutes); // Conecta suas rotas
 
+app.get('/', (req, res) => {
+    res.send('🚀 Servidor Node.js de Louise rodando com sucesso!');
+});
 app.listen(3000, () => console.log('Servidor Node rodando em http://localhost:3000'));
